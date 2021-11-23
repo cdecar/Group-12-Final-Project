5dd700035e65f818c0a9bbadf18d932c59e6c35d
cdecar <91353470+cdecar@users.noreply.github.com>
2021-11-22 23:15:21 -0800

    main page almost complete

--- js/page.js
+++ js/page.js
@@ -1,1 +1,24 @@
 
+$("h1").hover(function(){
+  $(this).css("color", "purple");
+}, function (){
+  $(this).css("color", "MediumSlateBlue");
+});
+
+$("p").hover(function(){
+  $(this).css("color", "DodgerBlue");
+}, function (){
+  $(this).css("color", "purple");
+});
+
+$("titleFrame").hover(function(){
+  $(this).css("background-color", "rgba(0,0,0,.25)");
+}, function (){
+  $(this).css("background-color", "rgba(0,0,0,.15)");
+});
+
+$("bodyFrame").hover(function(){
+  $(this).css("background-color", "rgba(0,0,0,.25)");
+}, function (){
+  $(this).css("background-color", "rgba(0,0,0,.15)");
+});
