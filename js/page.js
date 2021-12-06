268ae940eff061a66e8a9aa6bbdfb4bc20908025
cdecar <91353470+cdecar@users.noreply.github.com>
2021-12-06 04:05:36 -0800

    main page edits

--- js/page.js
+++ js/page.js
@@ -1,15 +1,4 @@
 
-function showTitle(){
-
-  var revealTitle = document.getElementById("shown");
-  if(revealTitle.style.visibility == "hidden"){
-    revealTitle.style.visibility = "visible";
-  }
-  else{
-    revealTitle.style.visibility = "hidden";
-  }
-}
-
 
 $("p").hover(function(){
   $(this).css("color", "white");
@@ -17,10 +6,10 @@ $("p").hover(function(){
   $(this).css("color", "Gray");
 });
 
-$("titleFrame").hover(function(){
-  $(this).css("background-color", "rgba(0,0,0,.25)");
+$(".titleFrame").hover(function(){
+  $(this).css("color", "white");
 }, function (){
-  $(this).css("background-color", "rgba(0,0,0,.15)");
+  $(this).css("color", "Gray");
 });
 
 $("bodyFrame").hover(function(){
